a07327831847387afe01ac372e45ddf7f1415aaf
Boxbox100 <jeddyranivo@gmail.com>
2023-03-21 11:01:00 +0300

    effacer tout

--- testtomcat/src/main/java/etu1873/framework/servlet/annota1.java
+++
@@ -1,9 +0,0 @@
-package etu1873.framework.servlet;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface annota1 {
-    String url();
-}
